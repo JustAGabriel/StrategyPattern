e78aaed3562c35b5b769b0ef0471c3aeaaac88a0
davidTigris <g.musset@mailbox.org>
2021-10-26 17:02:25 +0200

    Add project files.

---
+++ ISortingStrategy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StrategyPattern
+{
+    interface ISortingStrategy
+    {
+        IEnumerable<double> SortCollection(IEnumerable<double> list);
+    }
+
+    class SortAscending : ISortingStrategy
+    {
+        public IEnumerable<double> SortCollection(IEnumerable<double> list)
+        {
+            return list.OrderBy(x => x);
+        }
+    }
+
+    class SortDescending : ISortingStrategy
+    {
+        public IEnumerable<double> SortCollection(IEnumerable<double> list)
+        {
+            return list.OrderByDescending(x => x);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StrategyPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> numbers = new List<double>(){ 1.0, 5.5, 3.2, 4.1, 2.3 };
+            ISortingStrategy sortingStrategy;
+
+            sortingStrategy = new SortAscending();
+            var result = sortingStrategy.SortCollection(numbers);
+
+            Console.WriteLine("Sort numbers ascending: ");
+            Console.WriteLine(string.Join(", ", result));
+
+            sortingStrategy = new SortDescending();
+            var result2 = sortingStrategy.SortCollection(numbers);
+
+            Console.WriteLine("Sort numbers descending: ");
+            Console.WriteLine(string.Join(", ", result2));
+        }
+    }
+}
